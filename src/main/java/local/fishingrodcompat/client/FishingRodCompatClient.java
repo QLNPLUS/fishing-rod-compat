dee806603aa7c631cfc72c9b09bd416667c7e4f9
qlnx2012 <qlnx2012@user.noreply.gitee.com>
2026-09-17 12:04:45 +0800

    Keep AutoFish running behind Forge screens

--- src/main/java/local/fishingrodcompat/client/FishingRodCompatClient.java
+++ src/main/java/local/fishingrodcompat/client/FishingRodCompatClient.java
@@ -6,7 +6,6 @@ import local.fishingrodcompat.registry.CompatItems;
 import net.minecraft.client.renderer.item.ItemProperties;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -23,8 +22,6 @@ public final class FishingRodCompatClient {
 
     @SubscribeEvent
     public static void onClientSetup(FMLClientSetupEvent event) {
-        MinecraftForge.EVENT_BUS.addListener(AutoFishScreenCycle::onScreenOpening);
-        MinecraftForge.EVENT_BUS.addListener(AutoFishScreenCycle::onClientTick);
         event.enqueueWork(() -> {
             registerCastProperty(CompatItems.AQUACULTURE_IRON_FISHING_ROD);
             registerCastProperty(CompatItems.AQUACULTURE_GOLD_FISHING_ROD);
